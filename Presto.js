2b46821776475eb65008d707ed093b88bc7df0ee
EmanueleSca5 <emanuele.scagliarini05@gmail.com>
2026-04-17 18:56:50 +0200

    Filtri indipendenti

--- Presto.js
+++ Presto.js
@@ -1,12 +1,21 @@
 let navbar = document.querySelector(".navbar")
 let navLink = document.querySelectorAll(".nav-link")
 let logoNavbar = document.querySelector(".logoNavbar")
-
+let card = document.querySelectorAll(".card")
+let navbarToggler = document.querySelector(".navbar-toggler")
+let navbarCollapse = document.querySelector(".navbar-collapse")
+let num1 = document.querySelector("#num1")
+let num2 = document.querySelector("#num2")
+let num3 = document.querySelector("#num3")
+let swiperWrapper = document.querySelector(".swiper-wrapper")
+let swiperSlide = document.querySelector(".swiper-slide")
+let cardReview = document.querySelector(".card-review")
+// let imgCard = document.querySelectorAll(".img-card")
 console.log(window);
 window.addEventListener("scroll" , ()=>{
     let scrolled = window.scrollY;
 
-    if(scrolled > 0){
+    if(scrolled > 1){
         navbar.classList.remove("bg-black")
         navbar.classList.add("bg-yellow")
         navbar.style.height = "80px"
@@ -15,8 +24,10 @@ window.addEventListener("scroll" , ()=>{
             link.classList.remove("text-yl")
             link.classList.add("text-bk")
         })
+        navbarCollapse.classList.remove("bg-black")
 
-        logoNavbar.src = "logoscroll.png"
+        navbarCollapse.classList.add("bg-yellow")
+        logoNavbar.src = "./MEDIA/logoscroll.png"
         
 
     }else{
@@ -28,9 +39,161 @@ window.addEventListener("scroll" , ()=>{
             link.classList.remove("text-bk")
             link.classList.add("text-yl")
 
-        logoNavbar.src = "logo.png"
+            navbarCollapse.classList.add("bg-black")
+            navbarCollapse.classList.remove("bg-yellow")
+        logoNavbar.src = "./MEDIA/logo.png"
 
         })
     }
     
 })
+
+card.forEach((card)=>{
+    card.addEventListener("mouseenter", ()=>{
+        let img = card.querySelector(".img-card")
+        img.src = "./MEDIA/controller-red.png"
+    })
+    card.addEventListener("mouseleave", ()=>{
+        let img = card.querySelector(".img-card")
+        img.src = "./MEDIA/controller.png"
+    })
+})
+
+
+//   CHIAMATE ASINCRONE: vengono eseguite solo dopo l intera compilazione del foglio JS
+// setInterval(): crea un loop infinito in cui possiamo gestire la durata delle singole iterazioni
+// il setInterval(), è una funzione che vuole 2 parametri, il primo è la callback, il secondo è l intervallo di tempo (parte da millisecondi (1 = 1 millisecondo)).
+
+// clearInterval(): ha lo scopo di interrompere un setInterval()
+
+// setTimeout: Fa partire un blocco di istruzioni dopo tot secondi
+
+
+function createInterval(n, element, time){
+
+    let counter = 0;
+   let interval = setInterval(()=>{
+        
+    if(counter < n){
+        counter++
+    element.innerHTML = counter;
+    }else{
+        console.log("loop fermato");
+        clearInterval(interval)
+    }
+    
+}, time);
+
+// il setTimeout dice che qualcosa puo tornare ad un valore solo dopo tot secondi
+// ES: se confirm non è true, potrà tornare true solo dopo 12 secondi
+    setTimeout(()=>{
+        confirm = true;
+
+    }, 6000)
+
+} 
+
+
+
+// IntersectionObserver: è una classe del browser che si occupa di far scattare una funzione nel momento in cui sul browser sono visibili gli elementi html che noi gli indichiamo
+// new: è una keyword che mi permette di generare un oggetto partendo da una CLASSE
+let confirm = true;
+
+let observer = new IntersectionObserver((entries)=>{
+    // usiamo forEach poiche il parametro è un array
+    entries.forEach((entry)=>{
+        // .isIntersecting vuol dire "quando è stato intersecato/incontrato"
+        if(entry.isIntersecting && confirm){
+            createInterval(1300, num1, 3)
+            createInterval(300, num2, 12)
+           createInterval(400, num3, 11)
+           confirm = false
+        }
+
+    })
+});
+
+// .observe richiama tutta la intersectionObserver function
+observer.observe(num1)
+
+let reviews = [
+    {nome : "Matteo", titolo : "Il miglior sito del mondo!!", voto : 5, desc : "lorem ipsum"},
+    {nome : "Andrea", titolo : "Il peggior sito del mondo!!", voto : 2, desc : "lorem ipsum"},
+    {nome : "Lucia", titolo : "Prodotti molto buoni", voto : 4, desc : "lorem ipsum"},
+    {nome : "Luca", titolo : "Sono davver soddisfatto", voto : 5, desc : "lorem ipsum"},
+]
+
+reviews.forEach((recensione)=>{
+    let div = document.createElement("div")
+    div.classList.add("swiper-slide")
+
+    
+    div.innerHTML = `
+    <div class="card-review">
+    <p class="h3 nome-rec">${recensione.nome} </p>
+    <p class="h4 rec">${recensione.titolo} </p>
+    <div class="d-flex justify-content-center star">
+    
+    </div>
+    <p class="rec h5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cupiditate commodi possimus nesciunt, dolore quidem amet accusantium fugit explicabo numquam quas repellendus .</p>
+    </div>
+    `;
+    
+    swiperWrapper.appendChild(div)
+    
+    
+})
+let star = document.querySelectorAll(".star")
+
+star.forEach((stella, index)=>{
+    for(let i = 1; i <= reviews[index].voto; i++){
+        let icon = document.createElement("i")
+        icon.classList.add("fa-solid", "fa-star")
+        icon.style.color = "var(--yl)";
+        stella.appendChild(icon);
+    }
+
+    let difference = 5 - reviews[index].voto
+
+     for(let i = 1; i <= difference; i++){
+        let icon = document.createElement("i")
+        icon.classList.add("fa-regular", "fa-star")
+        icon.style.color = "var(--yl)";
+        stella.appendChild(icon);
+    }
+})
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay : {
+    delay : 5000,
+  },
+ effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
---
+++ annunci.js
@@ -0,0 +1,180 @@
+let navbar = document.querySelector(".navbar")
+let navLink = document.querySelectorAll(".nav-link")
+let logoNavbar = document.querySelector(".logoNavbar")
+let navbarToggler = document.querySelector(".navbar-toggler")
+let navbarCollapse = document.querySelector(".navbar-collapse")
+
+
+
+navbar.style.position = "sticky"
+window.addEventListener("scroll" , ()=>{
+    let scrolled = window.scrollY;
+    
+    if(scrolled > 1){
+        navbar.classList.remove("bg-black")
+        navbar.classList.add("bg-yellow")
+        navbar.style.height = "80px"
+        
+        navLink.forEach((link)=>{
+            link.classList.remove("text-yl")
+            link.classList.add("text-bk")
+        })
+        navbarCollapse.classList.remove("bg-black")
+        
+        navbarCollapse.classList.add("bg-yellow")
+        logoNavbar.src = "./MEDIA/logoscroll.png"
+        
+        
+    }else{
+        navbar.classList.remove("bg-yellow")
+        navbar.classList.add("bg-black")
+        navbar.style.height = "140px"
+        
+        navLink.forEach((link)=>{
+            link.classList.remove("text-bk")
+            link.classList.add("text-yl")
+            
+            navbarCollapse.classList.add("bg-black")
+            navbarCollapse.classList.remove("bg-yellow")
+            logoNavbar.src = "./MEDIA/logo.png"
+            
+        })
+    }
+    
+})
+
+// .json: JavaScript object Notification
+
+// API: chiavi che ci permettono di raggiungere un .json online
+
+// fetch(): Chiamata asincrona che ci permette di collegarci ad un JSON e da esso estrarne il dato sotto forma di Promise.
+
+// .then(): Questo metodo permette di convertire la promise nel dato strutturale e di poterlo utilizzare come tale in JS.
+
+// 1. fetch()= mi collego al json e ne ottengo una Promise
+// 2. .then()= converto la promise in un dato strutturale JS
+// 3. .then()= utilizzare il dato ottuenuto
+
+fetch("./annunci.json").then((response)=> response.json()).then((data)=>{
+    
+    let wrapperAcc = document.querySelector(".wrapperAcc")
+    let cardWrapper = document.querySelector("#cardWrapper")
+    
+    function radioCreate(){
+        let category = data.map((annuncio)=> annuncio.category);
+        console.log(category);
+        
+        
+        //    let categorieuniche = []
+        //     category.forEach((categoria)=>{
+            //     if(!categorieuniche.includes(categoria)){
+        //         categorieuniche.push(categoria)
+        //     }
+        //    })
+        //    console.log(categorieuniche);
+        
+        //    categorieuniche
+        
+        // Set(): Classe che mi restituisce, partendo da un array, un nuovo oggetto di tipo SET il quale contiene solo valori univoci
+        
+        let categorieuniche = Array.from(new Set(category))
+        console.log(categorieuniche);
+        
+        // essendo set un arraylike non possiamo usare i metodi degli array pewrcio per trasformalre un array like in un array possiamo usare Array.from():
+        
+        for(let i = 0; i < categorieuniche.length; i++){
+            let div = document.createElement("div")
+            div.classList.add("form-check")
+            div.innerHTML = `
+                <input class="form-check-input" type="radio" name="radioDefault" id="${categorieuniche[i]}">
+  <label class="form-check-label" for="${categorieuniche[i]}">
+    ${categorieuniche[i]}
+  </label>
+                `
+            wrapperAcc.appendChild(div)
+            
+        }
+    }
+        
+    radioCreate();
+        function showCards(array){
+            cardWrapper.innerHTML = " "
+            array.forEach((dato, i)=>{
+                let div = document.createElement("div")
+                div.classList.add("card-cst")
+                div.innerHTML = `
+                        <img src="https://picsum.photos/200/${300 + i}"
+                        <p class="h2" title="${dato.prodotto}">${dato.prodotto}</p>
+                        <p class="h5 text-start">${dato.category} </p>
+                        <p class="h4">Desc prod</p>
+                        <p class="h5"> ${dato.price}$</p>
+                    `
+                
+                cardWrapper.appendChild(div)
+            })
+        }
+    showCards(data)
+    
+    function filterByCategory(categoria){
+        // in questa funzione ho bisogno di ottnere un nuovo array partendo da data e gli elementi del nuovo array dovranno soddisfare kla condizione per la quale la loro category sia uguale alla categoria che stiamo passando alla funzione
+        if(categoria != "All"){
+
+            let filtered = data.filter((annuncio)=> annuncio.category == categoria)
+            showCards(filtered)
+        }else{
+            showCards(data)
+        }
+        
+    }
+    
+
+    let radiobuttons = document.querySelectorAll(".form-check-input")
+
+
+    radiobuttons.forEach((button)=>{
+        button.addEventListener("click", ()=>{
+            filterByCategory(button.id)
+            
+        })
+    })
+
+    let prezzo = document.querySelector(".prezzo")
+    let priceInput = document.querySelector("#price")
+
+    function setPriceInput(){
+        // Dopo aver catturato l input voglio settare come prorpieta max dello stesso il valore piu alto tra i price di ogni prodotto. Per farlo avro quindi bisogno di un array che contenga solo i prezzi, a quel punto lo ordino in maniera cresceente o descrescente e prendermi l elemento con il valore piu alto.
+
+        let prices = data.map((dato)=>+dato.price);
+        prices.sort((a, b)=> a - b)
+        let maxprice = Math.max(...prices)
+        console.log(maxprice);
+        
+        console.log(prices);
+
+        priceInput.max = maxprice;
+        priceInput.value = maxprice;
+        prezzo.innerHTML = maxprice;
+    }
+    setPriceInput()
+
+    function filterByPrice(){
+        let filtered = data.filter((dato)=> +dato.price <= priceInput.value);
+        showCards(filtered)
+        
+    }
+    priceInput.addEventListener("input", ()=>{
+        filterByPrice()
+        prezzo.innerHTML = priceInput.value
+    })
+
+    let wordInput = document.querySelector("#wordInput")
+    function filterByWord(parola){
+        let filtered = data.filter((dato)=> dato.prodotto.toLowerCase().includes(parola.toLowerCase()))
+        showCards(filtered)
+    }
+     
+    wordInput.addEventListener("input", ()=>{
+        filterByWord(wordInput.value)
+    })
+})
+
